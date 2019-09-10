1eb136cd7107102f8877fd076f2f848ce014d420
Justin <xhergz@gmail.com>
2019-09-10 08:28:16 -0400

    Added the missing AverageChangeFromPreviousDay field to the StockEntry class, added in StockEntry de-serializing tests, and renamed a few other tests that use string.Empty so it doesn't sound like it is using an empty JSON string.

--- AruaRoseToolSuiteLibrary_Tests/Data/StockEntryTestData.cs
+++ AruaRoseToolSuiteLibrary_Tests/Data/StockEntryTestData.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using AruaRoseToolSuiteLibrary.Api;
 using AruaRoseToolSuiteLibrary.Data;
 
 namespace AruaRoseToolSuiteLibrary_Tests.Data
 {
     public class StockEntryTestData
     {
-        public static DateTime ENTRY_DATE = DateTime.Now;
+        public static int STOCK_ENTRY_ID = 1;
 
-        public static int AVERAGE_PRICE = (int)PriceInfoTestData.LOW_SELL_PRICES.Average();
+        public static string ENTRY_DATE = "2019-01-01 00:00:00";
+
+        public static decimal AVERAGE_PRICE = (decimal)PriceInfoTestData.LOW_SELL_PRICES.Average();
 
         public static int LOWEST_PRICE = PriceInfoTestData.LOW_SELL_PRICES.Min();
 
@@ -20,11 +22,39 @@ namespace AruaRoseToolSuiteLibrary_Tests.Data
 
         public static int DATA_POINTS = PriceInfoTestData.LOW_SELL_PRICES.Count;
 
+        public static decimal AVERAGE_CHANGE_FROM_PREVIOUS_DAY = 7.14M;
+
+        public static string VALID_STOCK_ENTRY_WITH_AVERAGE_CHANGE_JSON = $@"{{
+            ""stockEntryId"": {STOCK_ENTRY_ID},
+            ""stockItemId"": {StockItemTestData.STOCK_ITEM_ID},
+            ""entryDate"": ""{ENTRY_DATE}"",
+            ""averagePrice"": {AVERAGE_PRICE},
+            ""lowestPrice"": {LOWEST_PRICE},
+            ""highestPrice"": {HIGHEST_PRICE},
+            ""dataPoints"": {DATA_POINTS},
+            ""averageChangeFromPreviousDay"": {AVERAGE_CHANGE_FROM_PREVIOUS_DAY}
+        }}";
+
+        public static string VALID_STOCK_ENTRY_WITHOUT_AVERAGE_CHANGE_JSON = $@"{{
+            ""stockEntryId"": {STOCK_ENTRY_ID},
+            ""stockItemId"": {StockItemTestData.STOCK_ITEM_ID},
+            ""entryDate"": ""{ENTRY_DATE}"",
+            ""averagePrice"": {AVERAGE_PRICE},
+            ""lowestPrice"": {LOWEST_PRICE},
+            ""highestPrice"": {HIGHEST_PRICE},
+            ""dataPoints"": {DATA_POINTS},
+            ""averageChangeFromPreviousDay"": null
+        }}";
+
+        public static string INVALID_STOCK_ENTRY_JSON = $@"{{
+            ""blah"": ""blah""
+        }}";
+
         public static StockEntry Generate()
         {
             return new StockEntry(
                 StockItemTestData.STOCK_ITEM_ID,
-                ENTRY_DATE,
+                DateTime.Parse(ENTRY_DATE),
                 AVERAGE_PRICE,
                 HIGHEST_PRICE,
                 LOWEST_PRICE,
---
+++ AruaRoseToolSuiteLibrary_Tests/StockEntry_Tests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+using AruaRoseToolSuiteLibrary.Api;
+using AruaRoseToolSuiteLibrary.Data;
+using AruaRoseToolSuiteLibrary_Tests.Data;
+
+namespace AruaRoseToolSuiteLibrary_Tests
+{
+    public class StockEntry_Tests
+    {
+        private StockEntry _stockEntry;
+
+        [Test]
+        public void JsonConstructor_WithValidJsonWithAverageChange_ReturnsValidStockEntry()
+        {
+            _stockEntry = JsonConvert.DeserializeObject<StockEntry>(StockEntryTestData.VALID_STOCK_ENTRY_WITH_AVERAGE_CHANGE_JSON);
+            Assert.AreEqual(StockEntryTestData.STOCK_ENTRY_ID, _stockEntry.StockEntryId);
+            Assert.AreEqual(StockItemTestData.STOCK_ITEM_ID, _stockEntry.StockItemId);
+            Assert.AreEqual(StockEntryTestData.ENTRY_DATE, _stockEntry.EntryDate.ToString(ArtsApi.DATE_FORMAT));
+            Assert.AreEqual(StockEntryTestData.AVERAGE_PRICE, _stockEntry.AveragePrice);
+            Assert.AreEqual(StockEntryTestData.HIGHEST_PRICE, _stockEntry.HighestPrice);
+            Assert.AreEqual(StockEntryTestData.LOWEST_PRICE, _stockEntry.LowestPrice);
+            Assert.AreEqual(StockEntryTestData.DATA_POINTS, _stockEntry.DataPoints);
+            Assert.AreEqual(StockEntryTestData.AVERAGE_CHANGE_FROM_PREVIOUS_DAY, _stockEntry.AverageChangeFromPreviousDay);
+        }
+
+        [Test]
+        public void JsonConstructor_WithValidJsonWithoutAverageChange_ReturnsValidStockEntry()
+        {
+            _stockEntry = JsonConvert.DeserializeObject<StockEntry>(StockEntryTestData.VALID_STOCK_ENTRY_WITHOUT_AVERAGE_CHANGE_JSON);
+            Assert.AreEqual(StockEntryTestData.STOCK_ENTRY_ID, _stockEntry.StockEntryId);
+            Assert.AreEqual(StockItemTestData.STOCK_ITEM_ID, _stockEntry.StockItemId);
+            Assert.AreEqual(StockEntryTestData.ENTRY_DATE, _stockEntry.EntryDate.ToString(ArtsApi.DATE_FORMAT));
+            Assert.AreEqual(StockEntryTestData.AVERAGE_PRICE, _stockEntry.AveragePrice);
+            Assert.AreEqual(StockEntryTestData.HIGHEST_PRICE, _stockEntry.HighestPrice);
+            Assert.AreEqual(StockEntryTestData.LOWEST_PRICE, _stockEntry.LowestPrice);
+            Assert.AreEqual(StockEntryTestData.DATA_POINTS, _stockEntry.DataPoints);
+            Assert.IsNull(_stockEntry.AverageChangeFromPreviousDay);
+        }
+
+        [Test]
+        public void JsonConstructor_WithInvalidJson_ReturnsValidStockEntry()
+        {
+            _stockEntry = JsonConvert.DeserializeObject<StockEntry>(StockEntryTestData.INVALID_STOCK_ENTRY_JSON);
+            Assert.AreEqual(0, _stockEntry.StockEntryId);
+            Assert.AreEqual(0, _stockEntry.StockItemId);
+            Assert.AreEqual(DateTime.MinValue, _stockEntry.EntryDate);
+            Assert.AreEqual(0, _stockEntry.AveragePrice);
+            Assert.AreEqual(0, _stockEntry.HighestPrice);
+            Assert.AreEqual(0, _stockEntry.LowestPrice);
+            Assert.AreEqual(0, _stockEntry.DataPoints);
+            Assert.IsNull(_stockEntry.AverageChangeFromPreviousDay);
+        }
+
+        [Test]
+        public void JsonConstructor_WithEmptyString_ReturnsNull()
+        {
+            _stockEntry = JsonConvert.DeserializeObject<StockEntry>(string.Empty);
+            Assert.IsNull(_stockEntry);
+        }
+    }
+}
